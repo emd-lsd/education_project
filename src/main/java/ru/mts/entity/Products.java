13e8bcf7f0744607b7b2faf793db15631f3f0fef
Dmitry <k9514833133@gmail.com>
2023-12-12 17:23:13 +0300

    Changings 1.3 (new exception at null)

--- src/main/java/ru/mts/entity/Products.java
+++ src/main/java/ru/mts/entity/Products.java
@@ -6,6 +6,8 @@ package ru.mts.entity;
     скидка;
  */
 
+import java.util.Optional;
+
 public class Products {
     // Количество товаров
     private int amount;
@@ -21,24 +23,39 @@ public class Products {
     -discount – скидка на товар
      */
     public Products(int amount, double price, double discount) {
-        this.amount = amount;
-        this.price = price;
-        this.discount = discount;
+        this.amount = Optional.of(amount).orElseThrow(() -> new IllegalArgumentException("Количество товаров не может быть пустым"));
+        this.price = Optional.of(price).orElseThrow(() -> new IllegalArgumentException("Цена не может быть пустой"));
+        this.discount = Optional.of(discount).orElseThrow(() -> new IllegalArgumentException("Скидка не может быть пустой"));
+        System.out.println(Products.this);
     }
+
+
 
     // Метод для посчёта общей суммы покпки со скидкой и без неё
     public static void calculateTotal(Products product){
 
-        if(product.amount <= 0 || product.price <= 0 || product.discount <0 || product.discount > 100){
-            System.out.println("Некорректные данные. Стоимость товара и количество должны быть больше 0. Скидка должна быть в диапазоне от 0 до 100");
-            System.exit(0);
+        // Инициализация объекта Optional
+        Optional<Products> prodExample = Optional.ofNullable(product);
+
+        // Проверка на null
+        if(prodExample.isPresent()){
+            // Проверка на некорректные данные
+            if(product.amount <= 0 || product.price <= 0 || product.discount <0 || product.discount > 100){
+                System.out.println("Некорректные данные. Стоимость товара и количество должны быть больше 0. Скидка должна быть в диапазоне от 0 до 100. Выход из программы!");
+                throw new IllegalArgumentException("Incorrect data");
+            }
+
+            double totalCost = product.amount * product.price; //Сумма покупки без скидки
+            double totalCostWithDisc = totalCost*(1-product.discount*0.01); // Сумма покупки со скидкой
+
+            System.out.println("Общая сумма покупки без скидки: "+String.format("%.2f", totalCost));
+            System.out.println("Общая сумма покупки со скидкой: "+String.format("%.2f",totalCostWithDisc));
+            System.out.println();
+        }
+        else {
+            throw new IllegalArgumentException("NOT NULL!");
         }
 
-        double totalCost = product.amount * product.price; //Сумма покупки без скидки
-        double totalCostWithDisc = totalCost*(1-product.discount*0.01); // Сумма покупки со скидкой
 
-        System.out.println("Общая сумма покупки без скидки: "+String.format("%.2f", totalCost));
-        System.out.println("Общая сумма покупки со скидкой: "+String.format("%.2f",totalCostWithDisc));
-        System.out.println();
     }
 }
--- src/main/java/ru/mts/service/Main.java
+++ src/main/java/ru/mts/service/Main.java
@@ -5,16 +5,22 @@ import ru.mts.entity.Products;
 public class Main {
     public static void main(String[] args) {
         // Создание новых объектов товаров
+
         Products prod1 = new Products(5, 200, 0.75);
         Products prod2 = new Products(30, 33, 42.575);
         Products prod3 = new Products(3, 10, 59.1);
-        //Products prod4 = new Products(5, 15, 101);
+        Products prod4 = new Products(0, 15, 101);
+
 
         // Расчёт общей суммы товаров для объектов
         Products.calculateTotal(prod1);
         Products.calculateTotal(prod2);
         Products.calculateTotal(prod3);
         //Products.calculateTotal(prod4);
+
+        Products.calculateTotal(null);
+
+
 
     }
 }
