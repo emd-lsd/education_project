83769e2799b596bdb1fbaacbd537bb0ee870ba62
Dmitry <k9514833133@gmail.com>
2024-02-19 18:33:03 +0300

    HW-8: yaml&properties

---
+++ src/test/java/ru/mts/AnimalsRepositoryTest.java
@@ -0,0 +1,16 @@
+package ru.mts;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest()
+@ActiveProfiles("test")
+public class AnimalsRepositoryTest {
+
+    @Test
+    public void test() {
+
+    }
+}
---
+++ src/test/java/ru/mts/TestConfig.java
@@ -0,0 +1,21 @@
+package ru.mts;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ActiveProfiles;
+import ru.mts.repository.AnimalsRepository;
+import ru.mts.repository.AnimalsRepositoryImpl;
+import ru.mtsstarter.service.CreateAnimalService;
+
+@TestConfiguration
+@ActiveProfiles("test")
+public class TestConfig {
+    @Autowired
+    CreateAnimalService createAnimalService;
+
+    @Bean
+    public AnimalsRepository animalsRepository(){
+        return new AnimalsRepositoryImpl(createAnimalService);
+    }
+}
